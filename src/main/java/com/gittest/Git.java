f5843bc9a803274ccf7265d69e4f7b12f70073e8
XiaoDong <2045961070@qq.com>
2022-08-12 21:27:10 +0800

    test第git次提交

--- src/main/java/com/gittest/Git.java
+++ src/main/java/com/gittest/Git.java
@@ -8,5 +8,6 @@ public class Git {
         System.out.println("hello hot-fix");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("hello git");
     }
 }
